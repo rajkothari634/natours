e2b6d7057891ec0d8466a97e608da9e8dbd0dfe7
Raj kothari <rkmeansrk39@gmail.com>
2020-01-25 02:11:24 +0530

    update

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'hello from server side', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('post method is running');
+// });
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Insertion Complete'
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find(el => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour
+    }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  console.log(req.body);
+  const newTour = Object.assign(
+    {
+      id: newId
+    },
+    req.body
+  );
+  tours.push(newTour);
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('app running on port ${port}...');
+});

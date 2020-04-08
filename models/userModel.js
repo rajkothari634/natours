14bf0519266c173588357de4b25dbff7e88d36e7
Raj kothari <rkmeansrk39@gmail.com>
2020-04-08 18:49:34 +0530

    update

---
+++ models/userModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    select: false,
+    type: String,
+    required: [true, 'password is needed'],
+    minlength: 8,
+  },
+  passwordconfirm: {
+    type: String,
+    require: [true, 'requierd confirm password'],
+    validate: {
+      // only on save and create
+      validator: function (el) {
+        return el === this.password;
+      },
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordconfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

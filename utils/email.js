9749a1e7ee59df27a965156e9c3d5ce727d4cb51
Raj kothari <rkmeansrk39@gmail.com>
2020-05-30 23:41:51 +0530

    update

--- utils/email.js
+++ utils/email.js
@@ -1,21 +1,23 @@
 const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
+  console.log('working ' + 'one');
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    service: 'Gmail',
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
   });
+  console.log('working ' + 'two');
   const mailOptions = {
-    from: 'raj kothari <rkmeansrk39@gmail.com>',
+    from: 'rkmeansrk39@gmail.com',
     to: options.email,
     subject: options.email,
     text: options.messsage,
   };
   await transporter.sendMail(mailOptions);
+  console.log('working ' + 'three');
 };
 
 module.exports = sendEmail;

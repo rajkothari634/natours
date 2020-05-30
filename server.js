8eeae97fa5b1a22ea30217c50293650a82745367
Raj kothari <rkmeansrk39@gmail.com>
2020-05-30 23:41:28 +0530

    update

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB successful'));
+  .then(() => console.log('DB successful'))
+  .catch((err) => {
+    console.log(err);
+  });
 
 // mongoose
 //   .connect(DB, {
